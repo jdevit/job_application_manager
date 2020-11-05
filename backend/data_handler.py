1f30591cfebf4476a9007ecba39abeea0d4755cb
jeremyt <jez_is_the_best@hotmail.com>
2020-11-05 23:40:24 +0000

    Datatables - modify and save

--- backend/data_handler.py
+++ backend/data_handler.py
@@ -22,31 +22,85 @@ class DataHandler(object):
 
     ## Only initialises gmailapi if necessary and not when data_handler.py initialised
     def initAutogmail(self):
+        ''' Initialises gmail api py '''
         if self.gmailapi is None:
             self.gmailapi = AutoGmail.getInstance()
 
     def gmail_showLabels(self):
+        ''' Test method to print something from the gmail api  '''
         self.gmailapi.showSomething()
 
     def gmail_showInbox(self):
         inbox = self.gmailapi.getInbox()
 
-    def saveData(self, role_title, company, date):
+    def saveNewData(self, role_title, company, date, website):
+        '''
+        Saves a new application given the following parameters
+        :param role_title: Title of the job role
+        :param company: Name of the company applied to
+        :param date: Date of application or last update
+        :param website: Website used where application was submitted at
+        :return:
+        '''
         # role_title = "role_title_example1"
         # company = "company_example1"
         # date = "date_example1"
-        job_data = DataFormatter.job_input2db(role_title, company, date)
+        # website = "website1"
+        # status = "waiting_reply"
+        job_data = DataFormatter.job_input2db(role_title, company, date, website, 'waiting for reply')
 
 
         self.database.save_data_job(job_data)
 
 
     def getData(self):
+        '''
+        Gets all job application data in database
+        :return: list of applications formatted in a dictionary
+        '''
+
         col_allJobs = self.database.get_data_job()
+
         listJobs = []
         for job in col_allJobs:
             listJobs.append(DataFormatter.job_db2output(job))
-        print(listJobs)
+        # print(listJobs)
 
         return listJobs
 
+
+    def getDataAsJson(self):
+        allJobs = {"data": []}
+
+        # Add headers
+        allJobs['header'] = ['id', 'role', 'company', 'date', 'website', 'status']
+
+        for job in self.database.get_data_job():
+            job_f = DataFormatter.job_db2output(job)
+            # job_data = [job_f['_id'], job_f['role'], job_f['company'], job_f['date'], job_f['date'], job_f['website'], job_f['status']]
+
+            allJobs['data'].append(job_f)
+
+        # Add checkbox
+        # for job in allJobs['data']:
+        #     job['checkbox_div'] = '<div class="checkbox"><input type="checkbox" value="'+job['_id']+'" onclick="toggleEnableDeleteButton()"></div>'
+
+        # Add Modify button
+        for job in allJobs['data']:
+            job['modify_button'] = '<div class="modify-data"><button class="btn btn-info btn-sm" onclick="modifyDataButton(this)">Modify</button></div> '
+            job['cancel_button'] = '<div class="cancel-data"><button class="btn btn-secondary btn-sm" onclick="cancelModifyButton(this)">Cancel</button></div> '
+            job['save_button'] = '<div class="save-data"><button class="btn btn-success btn-sm" onclick="saveDataButton(this)">Save</button></div> '
+            job['delete_button'] = '<div class="delete-data"><button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modalDelete" onclick="deleteDataButton(this)">Delete</button></div> '
+
+
+        return allJobs
+
+
+    def deleteData(self, listOfId):
+        for id in listOfId:
+            self.database.delete_one_data_job(id)
+
+    def updateData(self, id, new_values):
+        # values = [x.capitalize() for x in new_values]
+        formatted_values = DataFormatter.job_orderedList2dict(new_values)
+        self.database.update_one_data_job(id, formatted_values)
--- interface/interface.py
+++ interface/interface.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from backend.data_handler import DataHandler
+from log import Log
+import json
 
 
+# TODO: 1. Change all checkboxes to Modify button
+#       2. Bug: refresh toggle disable delete button if clicked next page of table
 class Interface(object):
 
     instance = None
@@ -25,26 +29,24 @@ class Interface(object):
 
 
 
-    def getAllData(self):
-        return self.dataHandler.getData()
-
-
 
     def setRoutes(self):
 
         @self.app.route('/')
         def home():
+            Log.save_to_log('[Interface.py] Link to home.html')
             return render_template('home.html')
 
         @self.app.route('/save')
         def link_to_save():
-            print("link_to_save()")
+            ''' Render template save.html '''
+            Log.save_to_log('[Interface.py] Link to save.html')
 
             return render_template('save.html')
 
         @self.app.route('/save/auto/gmail')
         def saving_gmail_data():
-            print("saving_gmail_data()")
+            Log.save_to_log('[Interface.py] saving gmail data')
             self.dataHandler.initAutogmail()
             self.dataHandler.gmail_showInbox()
 
@@ -52,30 +54,74 @@ class Interface(object):
 
         @self.app.route('/save/posting', methods=['POST'])
         def saving_data():
-            print("saving_data()")
+            Log.save_to_log('[Interface.py] Saving job application')
 
             if request.method == 'POST':
                 role = request.form['new-input-role-title']
                 company = request.form['new-input-company-name']
                 date = request.form['new-input-date']
-                print(role, company, date)
+                website = request.form['new-input-website']
+                if website == 'other':
+                    website = request.form['new-input-website-other']
 
-                ## Handle job data
-                self.dataHandler.saveData(role, company, date)
+                ## Save job data into database
+                self.dataHandler.saveNewData(role, company, date, website)
 
-                ## Returns a list of jobs
-                all_jobs = self.getAllData()
-                return render_template('view_jobs.html', all_jobs=all_jobs)
+                Log.save_to_log('[Interface.py] Saved job application.')
+                return render_template('home.html', saved='True')
+
+            Log.save_to_log('[Interface.py] Failed as request.method != POST')
 
 
-        @self.app.route('/saved_jobs')
-        def link_to_saved_jobs():
-            print("link_to_saved_jobs()")
+        @self.app.route('/view_jobs')
+        def link_to_view_jobs():
+            ''' Retrieves application data from database then renders template view_jobs.html '''
+            Log.save_to_log('[Interface.py] Link to view_jobs.html')
 
             ## Returns a list of jobs
-            all_jobs = self.getAllData()
+            # all_jobs = self.dataHandler.getData()
 
-            return render_template('view_jobs.html', all_jobs=all_jobs)
+            return render_template('view_jobs.html')
+
+
+        @self.app.route('/display_jobs_in_table', methods=['GET'])
+        def display_jobs_in_table():
+            ''' Method to display data to an ajax datatable '''
+            Log.save_to_log('[Interface.py] Displaying Jobs in Table')
+
+            ## Returns a list of jobs
+            all_jobs = self.dataHandler.getDataAsJson()
+
+            print(all_jobs)
+
+            return json.dumps(all_jobs)
+
+        @self.app.route('/delete_selected_data/<input_id_list>')
+        def delete_selected_data(input_id_list):
+            Log.save_to_log('[Interface.py] Deleting data:', input_id_list)
+
+            id_list = input_id_list.rstrip(',').split(',')
+            print(id_list)
+            self.dataHandler.deleteData(id_list)
+
+            return 'delete'
+
+        @self.app.route('/update_selected_data/<input_id>/<list_values>')
+        def update_selected_data(input_id, list_values):
+            Log.save_to_log('[Interface.py] Updating data:', input_id, list_values)
+
+            example_id = '5f948f8af2436672c82304fd'
+            example_list_values = []
+
+            new_list_values = list_values.split(',')
+            print("id:",input_id,"| List_values:",new_list_values)
+
+            ## list_values: list of new values given key (role:developer, company:apple)
+
+            self.dataHandler.updateData(input_id, new_list_values)
+
+            return 'modify'
+
 
 
 
@@ -86,9 +132,8 @@ class Interface(object):
         return render_template('footer.html')
 
     def run(self):
-
         try:
-            self.app.run(debug = True)
-            print("Running")
+            self.app.run(debug=True)
+            Log.save_to_log('[Interface.py] App is running. debug =', self.app.debug)
         except:
-            print("Not running")
+            Log.save_to_log('[Interface.py] App is not running. debug =', self.app.debug)
